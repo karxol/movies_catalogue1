673992c5b37d0c2c58c57f4bb3e2338b144f0578
Karol Kondracki <karxol@o2.pl>
2022-10-20 10:46:04 +0200

    Commit message

--- test/test_tmdb.py
+++ test/test_tmdb.py
@@ -1,6 +1,6 @@
-import tmdb_client, requests
+import tmdb_client
 from unittest.mock import Mock
-
+from main import app
 
 def test_get_poster_url_uses_default_size():
    # Przygotowanie danych
@@ -26,3 +26,13 @@ def test_get_movies_list(monkeypatch):
 
    movies_list = tmdb_client.get_movies_list(list_type="popular")
    assert movies_list == mock_movies_list
+
+
+def test_homepage(monkeypatch):
+   api_mock = Mock(return_value={'results': []})
+   monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+
+   with app.test_client() as client:
+       response = client.get('/')
+       assert response.status_code == 200
+       api_mock.assert_called_once_with('movie/popular','list_type')
